a2474ced06c9a7ef246359252e05bfe83293c9d2
Your Name <you@abc.net>
2020-06-01 12:06:25 +0600

    level 1, added card columns with advertisements.

---
+++ js/2gis.js
@@ -0,0 +1,9 @@
+var DG = require('2gis-maps');
+var map = DG.map('map', {
+    'center': [54.98, 82.89],
+    'zoom': 13
+});var DG = require('2gis-maps');
+var map = DG.map('map', {
+    'center': [54.98, 82.89],
+    'zoom': 13
+});
